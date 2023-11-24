58ea21866a3a6c2f6885baf01498adcffdcdcea0
KiranSatyaRaj <kiranjamy20021508@gmail.com>
2023-11-24 09:43:42 +0530

    add maze functionality

--- mazesolver/graphics.py
+++ mazesolver/graphics.py
@@ -1,29 +1,49 @@
 from tkinter import Tk, BOTH, Canvas
-from line import *
+
 
 class Window:
     def __init__(self, width, height):
-        self._width = width
-        self._height = height
-        self.__root_widget = Tk()
-        self.__root_widget.title("MazeSolver")
-        self._canvas = Canvas(width=self._width, height=self._height, background="black")
-        self._canvas.pack(fill="both")
+        self.__root = Tk()
+        self.__root.title("Maze Solver")
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
+        self.__canvas.pack(fill=BOTH, expand=1)
         self.__running = False
-        self.__root_widget.protocol("WM_DELETE_WINDOW", self.close)
 
     def redraw(self):
-        self.__root_widget.update_idletasks()
-        self.__root_widget.update()
+        self.__root.update_idletasks()
+        self.__root.update()
 
     def wait_for_close(self):
         self.__running = True
         while self.__running:
             self.redraw()
+        print("window closed...")
+
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.__canvas, fill_color)
 
     def close(self):
         self.__running = False
 
-    def draw_line(self, line, fill_color="white"):
-        self._line = line
-        self._line.draw(self._canvas, fill_color)
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Line:
+    def __init__(
+        self,
+        p1,
+        p2,
+    ):
+        self.p1 = p1
+        self.p2 = p2
+
+    def draw(self, canvas, fill_color="black"):
+        canvas.create_line(
+            self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2
+        )
+        canvas.pack(fill=BOTH, expand=1)
--- mazesolver/main.py
+++ mazesolver/main.py
@@ -1,8 +1,20 @@
-from graphics import *
-from line import *
-from cell import *
+from graphics import Window
+from maze import Maze
 
-win = Window(800, 600)
-c = Cell(win)
-c.draw(50, 50, 100, 100)
-win.wait_for_close()
+
+def main():
+    num_rows = 12
+    num_cols = 16
+    margin = 50
+    screen_x = 800
+    screen_y = 600
+    cell_size_x = (screen_x - 2 * margin) / num_cols
+    cell_size_y = (screen_y - 2 * margin) / num_rows
+    win = Window(screen_x, screen_y)
+
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win, 10)
+
+    win.wait_for_close()
+
+
+main()
